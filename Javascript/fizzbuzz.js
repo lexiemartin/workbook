dd1ff0182edcb85bfbf541dd0620a088bb2d1930
Lexie Martin <m.lexie.martin@gmail.com>
2015-06-24 20:27:07 -0600

    Add improved answer

--- Javascript/fizzbuzz.js
+++ Javascript/fizzbuzz.js
@@ -1,5 +1,5 @@
-// An implementation of the famous (infamous?) Fizzbuzz
-// problem. But the program says "Meow" and "Purr" instead
+// A solution to the famous (infamous?) Fizzbuzz
+// problem. The program says "Meow" and "Purr" instead
 // of "Fizz" and "Buzz" because I am a crazy cat lady.
 
 var totalCats = 100;
@@ -15,3 +15,19 @@ for (var i=1; i <= totalCats; i++) {
     console.log(i);
     }
 }
+
+// This was my original solution to the problem.
+// I looked up the question afterwards and discovered that
+// although my answer worked there's a better way to do it.
+
+// The following version is largely based on a post by
+// Trevor Dixon at http://stackoverflow.com/a/17623252
+
+var totalCats = 100;
+for (var i = 1; i <= totalCats; i++) {
+	var blank = '';
+	if (i % 3 === 0) blank += 'Meow';
+	if (i % 5 === 0) blank += 'Purr';
+	console.log(blank || i);
+	}
+
